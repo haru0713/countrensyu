45e401036e61adbd02cb5fa9ae1530ffbd110979
阿部遥人 <abeharu0713@gmail.com>
2015-05-09 09:08:01 +0900

    Initial Commit

---
+++ countrensyu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  countrensyu
+//
+//  Created by 阿部遥人 on 2015/05/09.
+//  Copyright (c) 2015年 阿部遥人. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
