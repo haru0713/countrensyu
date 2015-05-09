d6a40900859a9a3c00e0cd03d01bcf4bb6262177
阿部遥人 <abeharu0713@gmail.com>
2015-05-09 09:30:46 +0900

    あ

--- countrensyu/ViewController.swift
+++ countrensyu/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var n=0
+    @IBOutlet var l:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func p(){
+        n=n+1
+        l.text=String(n)
+    }
 
 }
 
